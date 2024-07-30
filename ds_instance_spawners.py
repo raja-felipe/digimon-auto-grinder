e3005491dcea352a4c9fb01ea65c08014299fcfa
Raja Felipe <ramonjaviervi@gmail.com>
2024-07-30 22:07:06 +1000

    Set multiprocessing to a main function

--- ds_instance_spawners.py
+++ ds_instance_spawners.py
@@ -5,8 +5,8 @@ def run_instance(instance_id):
     # Replace with the command to run your script
     subprocess.run(['python', 'src/ds_runner.py', str(instance_id)])
 
-if __name__ == "__main__":
-    num_instances = 40
+def spawn_instances():
+    num_instances = 5
     processes = []
 
     for i in range(num_instances):
@@ -16,3 +16,9 @@ if __name__ == "__main__":
 
     for p in processes:
         p.join()
+
+def main():
+    spawn_instances()
+
+if __name__ == "__main__":
+    main()
