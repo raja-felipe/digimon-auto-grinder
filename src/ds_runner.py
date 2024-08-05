acdb976306dd86b631ba2ef797e6ba90dd661d19
Raja Felipe <ramonjaviervi@gmail.com>
2024-08-05 17:03:53 +1000

    Input matching working, but some keys dont work

--- src/ds_runner.py
+++ src/ds_runner.py
@@ -1,10 +1,12 @@
-from desmume.emulator import DeSmuME
+from desmume.emulator import DeSmuME, SCREEN_HEIGHT, SCREEN_HEIGHT_BOTH, SCREEN_WIDTH
 import json
 import multiprocessing
 from playstyle import PlayStyle
 from randomPlayStyle import RandomPlayStyle
-from desmume.controls import keymask
+from desmume.controls import keymask, Keys
 import time
+from PIL import Image
+import inspect
 
 class DS_Runner:
     def __init__(self, playstyle: PlayStyle, dl_name=None):
@@ -14,22 +16,57 @@ class DS_Runner:
         self.game_instance = DeSmuME(dl_name)
         self.play_mode = playstyle
         self.prev_action = 1
+        self.window = None
         return
+    
+    def buffer_to_image(self, buffer):
+        """
+        Convert the display buffer (memoryview) to an image.
+        """
+        # Create an image from the buffer
+        image = Image.frombuffer('RGBA', (SCREEN_WIDTH, SCREEN_HEIGHT_BOTH), buffer, 'raw', 'RGBX', 0, 1)
+        return image
     
     def open_ds(self):
         self.game_instance.open(self.file_location)
         return
     
     def play_ds(self):
+        # for _ in range(180):
+        #     curr_action = self.get_action()
+        #     self.press_key(curr_action)
+        #     self.unpress_key()
+        # self.buffer_to_image(self.game_instance.display_buffer_as_rgbx()).show()
+
         window = self.game_instance.create_sdl_window()
-        
+        prev_input = Keys.KEY_NONE
+        self.unpress_all_keys()
+        self.game_instance._savestate.load(1)
+
+        # These are needed as you need to assign each frame individually
+        # to press and unpress buttons
+        FRAME_COUNT_TO_PRESS = 120
+        curr_frame_count = 0
+
         # Run the emulation as fast as possible until quit
         while not window.has_quit():
-            window.process_input()   # Controls are the default DeSmuME controls, see below.
-            curr_action = self.get_action()
-            self.press_key(curr_action)
+
+            window.process_input()
+
+            if curr_frame_count == FRAME_COUNT_TO_PRESS:
+                curr_action = self.play_mode.play()
+                print("PRESSED ACTION:", curr_action)
+                self.game_instance.input.keypad_add_key(curr_action)
+                prev_input = curr_action
+                curr_frame_count = 0
+            else:
+                if curr_frame_count == 0:
+                    print("UNPRESSING ACTION:", prev_input)
+                    self.game_instance.input.keypad_rm_key(prev_input)
+                curr_frame_count += 1
+            # print(f"CURR ACTION: {self.game_instance.input.keypad_get()}")  
+
             self.game_instance.cycle(with_joystick=False)
-            self.unpress_key()
             window.draw()
 
 
@@ -38,8 +75,8 @@ class DS_Runner:
         use this to press the button designated for the key
         """
         print(f"Going to press: {key}")
-        self.game_instance.input.keypad_add_key(keymask(key))
-        print(self.game_instance.input.keypad_get())
+        self.game_instance.input.keypad_add_key(key)
+        print("Current Keypad: ", self.game_instance.input.keypad_get())
         self.prev_action = key
         # curr_time = time.time()
         # while (time.time() - curr_time < time_pressed):
@@ -51,9 +88,18 @@ class DS_Runner:
         """
         use this to press the button designated for the key
         """
-        print(f"Going to press: {self.prev_action}")
-        self.game_instance.input.keypad_rm_key(keymask(self.prev_action))
+        print(f"Going to release: {self.prev_action}")
+        self.game_instance.input.keypad_rm_key(self.prev_action)
+        print("Current Keypad: ", self.game_instance.input.keypad_get())
         return
+    
+    def unpress_all_keys(self):
+        # print(inspect.getmembers(Keys()))
+        for key in inspect.getmembers(Keys()):
+            key_name = key[0]
+            key_val = key[1]
+            if not key_name.startswith("_") and key_name != "NB_KEYS" and key_name != "NO_KEY_SET":
+                self.game_instance.input.keypad_rm_key(key_val)
     
     def get_action(self) -> int:
         return self.play_mode.play()
@@ -65,6 +111,5 @@ if __name__ == "__main__":
     random_playstyle = PlayStyle()
     ds_runner = DS_Runner(playstyle=random_playstyle)
     ds_runner.open_ds()
+    # print(ds_runner.game_instance.is_running())
     ds_runner.play_ds()
-    # ds_runner_2 = DS_Runner()
-    # ds_runner_2.open_ds()
--- src/playstyle.py
+++ src/playstyle.py
@@ -3,11 +3,23 @@ from desmume.emulator import DeSmuME
 
 class PlayStyle:
     def __init__(self):
+        self.prev_played = Keys.KEY_B
+        self.loop_button = Keys.KEY_B
+
+        self.input_index = 0
+        self.max_inputs = 4
         return
     
     def play(self) -> int:
         """
         play returns the action the emulator should do
         """
-        print(Keys.KEY_A)
-        return Keys.KEY_A
+        if self.input_index == self.max_inputs:
+            self.input_index = 0
+
+        controls_sequence = [Keys.KEY_A, Keys.KEY_UP, Keys.KEY_DOWN, Keys.KEY_B]
+        play_key = controls_sequence[self.input_index]
+        self.input_index += 1
+        
+        # print(play_key)
+        return play_key
